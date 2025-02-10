bc7752a9e4133c6c202549d835b4d01a53d9f882
Orthalange Tharushi Madubhashinie <159515567+Madubhashinie@users.noreply.github.com>
2025-02-10 16:44:29 +0530

    Connect to database

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,13 @@
 
 import express from 'express';
 
-const app = express();
+import 'dotenv/config.js';
 import { productRouter } from './api/products.js';
 import globalErrorHandlingMiddleware from './api/middleware/global-error-handling-middleware.js';
+import { connectDB } from './infrastructure/db.js';
+import { categoryRouter } from './api/category.js';
 
+const app = express();
 app.use(express.json()); // For parsing JSON requests
 
 /*app.use((req, res, next) =>{
@@ -23,6 +26,8 @@ app.use(express.json()); // For parsing JSON requests
 //app.patch('/product/:id', updateProduct)
 
 app.use('/api/products', productRouter);
+app.use("/api/categories", categoryRouter);
 app.use(globalErrorHandlingMiddleware);
 
+connectDB();
 app.listen(8000, () => console.log(`Server running on port ${8000}`));
